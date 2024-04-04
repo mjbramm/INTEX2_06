0086287841950823c444de00708272d1aeb0441d
crf36 <149890484+crf36@users.noreply.github.com>
2024-04-04 10:18:15 -0600

    Fixed Index page

--- INTEX2_06/Controllers/HomeController.cs
+++ INTEX2_06/Controllers/HomeController.cs
@@ -19,48 +19,6 @@ namespace INTEX2_06.Controllers
             _repo = temp;
         }
 
-
-        //public IActionResult Index(int pageNum, string? bookCategory)
-        //{
-        //    int pageSize = 10;
-
-        //    var blah = new BooksListViewModel
-        //    {
-        //        Books = _repo.Books
-        //            .Where(x => x.Category == bookCategory || bookCategory == null)
-        //            .OrderBy(x => x.Title)
-        //            .Skip((pageNum - 1) * pageSize)
-        //            .Take(pageSize),
-
-        //        PaginationInfo = new PaginationInfo
-        //        {
-        //            CurrentPage = pageNum,
-        //            ItemsPerPage = pageSize,
-        //            TotalItems = bookCategory == null ? _repo.Books.Count() : _repo.Books.Where(x => x.Category == bookCategory).Count()
-
-        //        },
-
-        //        CurrentBookCategory = bookCategory
-        //    };
-
-        //    return View(blah);
-        //}
-
-        //[Authorize]
-        ////[Authorize(Roles = "Manager")]
-        //public async Task<IActionResult> Index(int pageNum, string? bookCategory)
-        //{
-        //    AppUser user = await userManager.GetUserAsync(HttpContext.User);
-        //    string message = "Hello " + user.UserName;
-
-        //    return View((object)message);
-        //}
-
-
-
-
-
-
         [Authorize]
         public async Task<IActionResult> Index(int pageNum, string? bookCategory)
         {
@@ -68,8 +26,6 @@ namespace INTEX2_06.Controllers
             if (User.Identity.IsAuthenticated)
             {
                 AppUser user = await userManager.GetUserAsync(HttpContext.User);
-                string message = "Hello " + user.UserName;
-                return View((object)message);
             }
             
             int pageSize = 10;
@@ -92,11 +48,8 @@ namespace INTEX2_06.Controllers
 
                 CurrentBookCategory = bookCategory
             };
-
             return View(blah);
         }
-
-
 
         public async Task<IActionResult> Privacy()
         {
