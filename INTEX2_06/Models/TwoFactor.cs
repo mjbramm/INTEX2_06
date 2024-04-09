e9abced5e5436b32c874e41890653f2eb866fc85
crf36 <149890484+crf36@users.noreply.github.com>
2024-04-09 13:18:26 -0600

    Deleted uneeded files

--- INTEX2_06/Models/TwoFactor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace INTEX2_06.Models
-{
-    public class TwoFactor
-    {
-        [Required]
-        public string TwoFactorCode { get; set; }
-    }
-}
